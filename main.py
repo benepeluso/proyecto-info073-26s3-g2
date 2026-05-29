57600d9217f7a8f000f26c4e2464cff928e089ac
JeanAlexx <jeanatorrealbahernadez@gmail.com>
2026-05-29 12:25:59 -0400

    implemento de mecanicas

--- main.py
+++ main.py
@@ -26,18 +26,30 @@ RETRASO = 200
 
 # Códigos de cada elemento del tablero
 VACIO = 0
-OBSTACULO = 1
+OBSTACULO = 67
 JUGADOR = 2
-MANZANA = 3
+MANZANA = 3*2
 
 # Tamaño del tablero
 # Si se cambian estas constantes, se debe modificar la definición
 # del tablero que se encuentra en función reiniciar().
 FILAS = 15
 COLUMNAS = 15
+#configuracion de obstaculos 
+CANT_OBSTACULOS=10
+# Cuantas manzanas se deben comer para ganar
+MANZANAS_PARA_GANAR = 5
 
 
-def aparecer_aleatorio(tablero, id_elem):
+#celda que conforman el borde del tablero
+BORDE = (
+[( c , 0) for c in range ( COLUMNAS ) ]
++ [( c , FILAS - 1) for c in range ( COLUMNAS ) ]
++ [(0 , f ) for f in range (1 , FILAS - 1) ]
++ [( COLUMNAS - 1 , f ) for f in range (1 , FILAS - 1) ]
+)
+
+def aparecer_aleatorio(tablero, id_elem , incluir_borde=True):
     """
     Coloca un elemento en una casilla vacía aleatoria del tablero.
 
@@ -59,13 +71,15 @@ def aparecer_aleatorio(tablero, id_elem):
     for fila in range(FILAS):
         for columna in range(COLUMNAS):
             # Obtenemos el elemento que se encuentra en esa fila y columna.
-            elem_pos = tablero[fila][columna]
-
-            if elem_pos == VACIO:
+            #elem_pos = tablero[fila][columna]
+            if tablero[fila][columna]==VACIO:
+                vacios.append((columna , fila))
+            #if elem_pos == VACIO:
                 # Al utilizar los paréntesis () dentro de la función, lo estaremos
                 # añadiendo como una tupla con la estructura (columna, fila).
-                vacios.append((columna, fila))
-
+             #   vacios.append((columna, fila))
+    if not incluir_borde:
+        vacios = [pos for pos in vacios if pos not in BORDE]
     # También se puede utilizar comprensión de listas para rellenar el arreglo
     # a la vez que lo recorremos:
     #
@@ -98,7 +112,8 @@ def poblar_tablero(tablero):
     Parámetros:
         - tablero: El tablero con sus posiciones actuales.
     """
-    aparecer_aleatorio(tablero, OBSTACULO)
+    for i in range(CANT_OBSTACULOS):
+        aparecer_aleatorio(tablero, OBSTACULO , incluir_borde=False)
     aparecer_aleatorio(tablero, MANZANA)
 
 
@@ -210,7 +225,7 @@ def cambiar_direccion(keys, direccion_actual):
     return direccion_actual
 
 
-def avanzar(tablero, pos_jugador, direccion):
+def avanzar( tablero , pos_jugador , direccion , manzanas_comidas ) :
     """
     Avanza el jugador un paso en la dirección dada.
 
@@ -238,22 +253,32 @@ def avanzar(tablero, pos_jugador, direccion):
 
     # Verificamos que no haya choque con el borde del tablero.
     if not (0 <= ind_nueva_col < COLUMNAS and 0 <= ind_nueva_fila < FILAS):
-        return "derrota", pos_jugador
+        return "derrota", pos_jugador , manzanas_comidas
 
     # Obtenemos el elemento que se encuentre en el tablero en la nueva posición del jugador.
     pos_elem = tablero[ind_nueva_fila][ind_nueva_col]
 
     if pos_elem == OBSTACULO:
-        return "derrota", pos_jugador
+        return "derrota", pos_jugador , manzanas_comidas
 
-    if pos_elem == MANZANA:
-        return "victoria", (ind_nueva_col, ind_nueva_fila)
+    if pos_elem == MANZANA :
+        manzanas_comidas += 1
+        # Mover al jugador a la nueva casilla
+        tablero [ ind_actual_fila ][ ind_actual_col ] = VACIO
 
+        tablero [ ind_nueva_fila ][ ind_nueva_col ] = JUGADOR
+        
+        # Si llegamos al objetivo , victoria
+        if manzanas_comidas >= MANZANAS_PARA_GANAR :
+            return "victoria", ( ind_nueva_col , ind_nueva_fila ) , manzanas_comidas
+        # Si no , generar otra manzana y continuar
+        aparecer_aleatorio ( tablero , MANZANA )
+        return "ok", ( ind_nueva_col , ind_nueva_fila ) , manzanas_comidas
     # Movimiento normal, si es que no encontramos manzana ni obstáculo.
     tablero[ind_actual_fila][ind_actual_col] = VACIO
     tablero[ind_nueva_fila][ind_nueva_col] = JUGADOR
 
-    return "ok", (ind_nueva_col, ind_nueva_fila)
+    return "ok", ( ind_nueva_col , ind_nueva_fila ) , manzanas_comidas
 
 
 def reiniciar():
@@ -351,6 +376,8 @@ def main():
     pos_jugador = (0, 0)
     direccion = (0, 0)
     tiempo_ultimo_mov = 0
+    
+    manzanas_comidas = 0
 
     mostrar_pantalla(screen, PANTALLA_INICIO)
 
@@ -368,6 +395,7 @@ def main():
                 if estado == ESTADO_INICIO:
                     if evento.key == pygame.K_SPACE:
                         tablero, pos_jugador = reiniciar()
+                        manzanas_comidas = 0
                         direccion = (0, 0)
                         # Obtiene tiempo en milisegundos
                         tiempo_ultimo_mov = pygame.time.get_ticks()
@@ -402,7 +430,7 @@ def main():
             # La variable RETRASO hace que si no han pasado esa cantidad de ticks,
             # entonces no se avanzará en el tablero.
             if direccion != (0, 0) and tiempo_actual - tiempo_ultimo_mov >= RETRASO:
-                resultado, pos_jugador = avanzar(tablero, pos_jugador, direccion)
+                resultado , pos_jugador , manzanas_comidas = avanzar(tablero , pos_jugador , direccion , manzanas_comidas)
 
                 if resultado == "derrota":
                     estado = ESTADO_DERROTA
